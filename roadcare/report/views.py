a95b63a51a6bc0f49ff8ea2107ed7a5cda99f335
APARNA SAJEEVAN <@parna@2005>
2026-03-04 00:02:20 +0530

    Revert "Merge branch 'main' of https://github.com/ardra-p/RoadCareReport"
    This reverts commit a62972746c7e0dcae1661944a7e6dff6260c6bed, reversing
    changes made to 50cefa2f8f5efc4f0432c6d322c8bb66a8dc75f1.
    
    git merge --abort#

--- roadcare/report/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def homepage(request):
-    return render(request,'home/home.html')
-
-def admin_login(request):
-    return render(request,'home/admin_login.html')
